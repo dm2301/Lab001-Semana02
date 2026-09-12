698ee56117956c26b744e415fbdf532104bdd838
D Medina <dmedina@example.com>
2026-09-12 23:46:50 +0000

    feat: agregar soporte para variable de entorno PORT

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 const MESSAGE = process.env.MESSAGE || 'Mundo';
 
 app.get('/', (req, res) => {
   res.send(`Hola ${MESSAGE}`);
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
